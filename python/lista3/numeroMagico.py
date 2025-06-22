eabbeda21185b8f97e5c0fdc1ae435981ec65431
dynorph <erickmsilva19@gmail.com>
2025-06-22 09:36:53 -0300

    refactor: python lista 3

--- python/lista3/numeroMagico.py
+++ python/lista3/numeroMagico.py
@@ -1,25 +1,33 @@
 def primo(num):
-  if num <= 1:
-    return False
+  if num <= 1: return False
+
   for i in range(2, int(num**0.5)+1):
-    if num % i == 0:
-      return False
+    if num % i == 0: return False
+
   return True
 
 def soma_digitos(num):
+  digits = str(num)
   soma = 0
-  for digit in num:
-    soma += digit
+
+  for digit in digits:
+    soma += int(digit)
   return soma
 
-num1 = int(input("Insira o primeiro numero (inicio do intervalo): "))
-num2 = int(input("Insira o segundo numero (fim do intervalo): "))
-contador = 0
 
-for i in range (num1, num2+1):
-  if i % 4 == 0 and primo(i) and soma_digitos(i) % 2 != 0:
-    contador += 1
-if contador > 0:
-  print("Há", contador, "números mágicos no intervalo.")
+num1 = int(input("Insira o número de início do intervalo: "))
+num2 = int(input("Insira o número de fim do intervalo: "))
+
+if(num1 > num2):
+  print("Erro: valor de início maior que valor de fim")
 else:
-  print("Não há números mágicos no intervalo.")
+  contador = 0
+
+  for i in range (num1, num2+1):
+    if i % 4 == 0 and primo(i) and soma_digitos(i) % 2 == 0: 
+      contador += 1
+
+  match contador:
+    case 0: print("Não há números mágicos no intervalo.")
+    case 1: print("Há 1 número mágico no intervalo.")
+    case _: print(f"Há {contador} números mágicos no intervalo.")
