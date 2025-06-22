21849eeec4a450ccd9d8ce690458429931328e27
dynorph <erickmsilva19@gmail.com>
2025-06-22 11:21:42 -0300

    refactor: python bingo

--- python/lista3/bingo.py
+++ python/lista3/bingo.py
@@ -1,40 +1,67 @@
 import random
 
+def menu(n):
+  print("1. Visualizar regras")
+  print("2. Gerar cartela")
+  print("3. Visualizar cartela")
+  print("4. Novo sorteio")
+  print("5. Sair")
+  print(f"Sorteios restantes: {n}")
 
-def imprime_cartela(cartela):
-  print("[", end="")
-  for int in cartela:
-    if int == cartela[len(cartela)-1]:
-      print(int,end="")
-    else:
-      print(int,end=", ")
-  print("]")
+print("--- BINGO ---")
 
-i = 0
-contador = 0
-cartela = []
+op = 0
+sorteios = 50
+cartela = set()
+temCartela = False
+numeros = []
 
-while(i < 5):
-  num = random.randint(1,75)
-  cartela.append(num)
-  i += 1
+for i in range (1, 75):
+  numeros.append(i)
 
-print("Sua cartela: ", end="")
-imprime_cartela(cartela)
+while op != 5 and sorteios > 0:
+  menu(sorteios)
 
-while(1):
-  if (len(cartela) == 0):
-    break
-  contador += 1
-  num_sorteado = random.choice(cartela)
-  print("Tente adivinhar o numero sorteado: ", end="")
-  while(1):
-    tentativa = int(input())
-    if tentativa != num_sorteado:
-      print("Numero errado! Tente novamente: ", end="")
-    else:
+  op = int(input("Escolha a opção desejada: "))
+
+  match op:
+    case 1: 
+      print("Neste jogo, você vai receber uma cartela com cinco números aleatórios diferentes de 1 a 75.")
+      print("Você terá 50 sorteios para tentar completar sua cartela. Se conseguir, você vence!")
+    case 2:
+      if len(cartela): print("Vocẽ já tem uma cartela!")
+      else:
+        while(len(cartela) < 5):
+          sorteado = random.choice(numeros)
+          cartela.add(sorteado)
+
+        print(f"Cartela gerada com sucesso! Sua cartela: {cartela}")
+        temCartela = True
+    case 3:
+      if temCartela: print(cartela)
+      else: print("Você não tem uma cartela para visualizar!")
+    case 4:
+      if temCartela:
+        sorteado = random.choice(numeros)
+        numeros.remove(sorteado)
+        print(f"O número sorteado nessa rodada foi... {sorteado}!")
+
+        if sorteado in cartela:
+          cartela.remove(sorteado)
+          print("O número estava na sua cartela!")
+          if len(cartela) != 0: print(f"Cartela atual: {cartela}")
+        else: print("O número não estava na sua cartela...")
+
+        sorteios -= 1
+
+        if len(cartela) == 0: break
+      else: print("Você precisa de uma cartela para jogar!")
+    case 5: 
+      print("Tchau!")
       break
-  cartela.remove(num_sorteado)
-  print("Voce acertou! Numeros restantes na cartela: ", end="")
-  imprime_cartela(cartela)
-print("Parabens! Você concluiu sua cartela em", contador, "sorteios")
+    case _: print("Erro: opção inválida!")
+
+if op != 5:
+  if sorteios > 0 and len(cartela) == 0:
+    print(f"Parabéns! Você ganhou o jogo em {50 - sorteios} rodadas!")
+  else: print(f"Você não conseguiu completar sua cartela... Obrigado por jogar!")
